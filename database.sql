01cd7da7c99b1f5ea857a01d49446e2f4dee10a4
anashm <anashammaoui07@gmail.com>
2025-01-15 15:23:28 +0100

    complete sql table creation

---
+++ database.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE youdemy;
+USE youdemy;
+CREATE TABLE users(
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
+	full_name VARCHAR(50),
+	email VARCHAR(50),
+	user_password VARCHAR(255),
+	user_role ENUM('admin','teacher','student')
+);
+CREATE TABLE categories(
+	category_id int PRIMARY KEY AUTO_INCREMENT,
+	category_name VARCHAR(50),
+	admin_id INT,
+	FOREIGN KEY (admin_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+CREATE TABLE courses(
+	course_id INT PRIMARY KEY AUTO_INCREMENT,
+	course_name VARCHAR(70),
+	course_type ENUM('video', 'document'),
+	teacher_id INT,
+	category_id INT,
+	FOREIGN KEY (teacher_id) REFERENCES users(user_id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+CREATE TABLE tags(
+	tag_id INT PRIMARY KEY AUTO_INCREMENT,
+	tag_name varchar(20)
+);
+CREATE TABLE coursTags(
+	tag_id INT,
+	course_id INT,
+	PRIMARY KEY (tag_id, course_id),
+	FOREIGN KEY (tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE,
+	FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
+);
